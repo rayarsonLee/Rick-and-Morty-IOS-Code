388b6ebe7adb4c771276a2a8d6d6d8a31501473f
lijiaxin2333 <lijiaxin.2333@bytedance.com>
2024-08-17 12:25:32 +0800

    Initial Commit

---
+++ RM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RM
+//
+//  Created by ljx on 2024/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
